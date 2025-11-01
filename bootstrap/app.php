db61cd94cc14c9a689411b345d45f00096a97d39
Sarvar Jafarov <sarvar.jafarov@gmail.com>
2025-10-31 22:34:59 -0400

    Trust Heroku proxies for HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');  // Trust all proxies (Heroku)
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureIsAdmin::class,
         ]);
