c04ffe4fe7a485cac6a16f64cad474666f235e8e
Sarvar Jafarov <sarvar.jafarov@gmail.com>
2025-11-25 22:52:03 -0500

    l

--- resources/views/components/workspace-pagination.blade.php
+++ resources/views/components/workspace-pagination.blade.php
@@ -1,62 +1,59 @@
-@php
-    $isPaginator = $paginator instanceof \Illuminate\Pagination\LengthAwarePaginator
-        || $paginator instanceof \Illuminate\Pagination\Paginator;
-    $elements = $isPaginator ? $paginator->elements() : [];
-@endphp
-
-@if ($isPaginator && $paginator->hasPages())
-    <div class="workspace-pagination">
-        <div class="workspace-pagination__meta">
-            Showing
-            <strong>{{ $paginator->firstItem() }}</strong>
-            to
-            <strong>{{ $paginator->lastItem() }}</strong>
-            of
-            <strong>{{ $paginator->total() }}</strong>
-            results
-        </div>
-        <div class="workspace-pagination__controls">
-            @if ($paginator->onFirstPage())
-                <span class="page-btn disabled">
-                    <span class="mdi mdi-arrow-left"></span>
-                    Previous
-                </span>
-            @else
-                <a class="page-btn" href="{{ $paginator->previousPageUrl() }}" rel="prev">
-                    <span class="mdi mdi-arrow-left"></span>
-                    Previous
-                </a>
-            @endif
-
-            <div class="page-numbers">
-                @foreach ($elements as $element)
-                    @if (is_string($element))
-                        <span class="page-number disabled">{{ $element }}</span>
-                    @endif
-
-                    @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                            @if ($page == $paginator->currentPage())
-                                <span class="page-number active">{{ $page }}</span>
-                            @else
-                                <a class="page-number" href="{{ $url }}">{{ $page }}</a>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
+@if (($paginator instanceof \Illuminate\Pagination\LengthAwarePaginator) || ($paginator instanceof \Illuminate\Pagination\Paginator))
+    @php($elements = $paginator->elements())
+    @if ($paginator->hasPages())
+        <div class="workspace-pagination">
+            <div class="workspace-pagination__meta">
+                Showing
+                <strong>{{ $paginator->firstItem() }}</strong>
+                to
+                <strong>{{ $paginator->lastItem() }}</strong>
+                of
+                <strong>{{ $paginator->total() }}</strong>
+                results
             </div>
+            <div class="workspace-pagination__controls">
+                @if ($paginator->onFirstPage())
+                    <span class="page-btn disabled">
+                        <span class="mdi mdi-arrow-left"></span>
+                        Previous
+                    </span>
+                @else
+                    <a class="page-btn" href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                        <span class="mdi mdi-arrow-left"></span>
+                        Previous
+                    </a>
+                @endif
 
-            @if ($paginator->hasMorePages())
-                <a class="page-btn" href="{{ $paginator->nextPageUrl() }}" rel="next">
-                    Next
-                    <span class="mdi mdi-arrow-right"></span>
-                </a>
-            @else
-                <span class="page-btn disabled">
-                    Next
-                    <span class="mdi mdi-arrow-right"></span>
-                </span>
-            @endif
+                <div class="page-numbers">
+                    @foreach ($elements as $element)
+                        @if (is_string($element))
+                            <span class="page-number disabled">{{ $element }}</span>
+                        @endif
+
+                        @if (is_array($element))
+                            @foreach ($element as $page => $url)
+                                @if ($page == $paginator->currentPage())
+                                    <span class="page-number active">{{ $page }}</span>
+                                @else
+                                    <a class="page-number" href="{{ $url }}">{{ $page }}</a>
+                                @endif
+                            @endforeach
+                        @endif
+                    @endforeach
+                </div>
+
+                @if ($paginator->hasMorePages())
+                    <a class="page-btn" href="{{ $paginator->nextPageUrl() }}" rel="next">
+                        Next
+                        <span class="mdi mdi-arrow-right"></span>
+                    </a>
+                @else
+                    <span class="page-btn disabled">
+                        Next
+                        <span class="mdi mdi-arrow-right"></span>
+                    </span>
+                @endif
+            </div>
         </div>
-    </div>
+    @endif
 @endif
