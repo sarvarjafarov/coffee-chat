36b76be14f6d70531a3bbda1a1e69dbaa1f97a5d
Sarvar Jafarov <sarvar.jafarov@gmail.com>
2025-12-09 12:58:27 -0500

    favic

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -166,10 +166,11 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('coffeechat-os-favicon.png?v=3') }}">
-        <link rel="icon" type="image/svg+xml" sizes="any" href="{{ asset('favicon.svg?v=3') }}">
-        <link rel="shortcut icon" href="{{ asset('coffeechat-os-favicon.png?v=3') }}">
-        <link rel="apple-touch-icon" href="{{ asset('coffeechat-os-favicon.png?v=3') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('coffeechat-os-favicon.png?v=4') }}">
+        <link rel="icon" type="image/svg+xml" sizes="any" href="{{ asset('favicon.svg?v=4') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico?v=4') }}">
+        <link rel="shortcut icon" href="{{ asset('coffeechat-os-favicon.png?v=4') }}">
+        <link rel="apple-touch-icon" href="{{ asset('coffeechat-os-favicon.png?v=4') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css">
         @php
